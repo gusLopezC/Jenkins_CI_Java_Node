fa55ef2941f0bde28f92b91d8508a27ad3412265
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 10:12:06 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -1,24 +1,12 @@
 job("git-pull"){
   description("This job will pull our files from github")
   scm{
-    github('dheeth/kube-web', 'master')
+    github('gusLopezC/Jenkins_kube-web', 'main')
   }
   triggers{
     githubPush()
   }
   steps{
-    shell('''
-ssh ec2-user@172.31.74.187 '
-path="/home/ec2-user/jenkins_vol/workspace/git-pull/"
-files=$(ls $path | grep -e php -e html | wc -l)
-if [[ $files > 0 ]]
-then
-cp $path* /home/ec2-user/master
-else
-echo "No php or html files received"
-exit 1
-fi
-'
-''')
+   
   }
 }
