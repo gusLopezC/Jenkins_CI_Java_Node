79463dff3ac9dc3b4288bbd129b33ba0bd7ca49d
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 10:29:34 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -1,16 +1,15 @@
-job("git-pull"){
-  description("This job will pull our files from github")
-  git {
-			remote {
-				github("gusLopezC/Jenkins_kube-we", "https")
-				credentials("29a0e27f-3839-46d4-a1a4-6e7fcead986d")
-			}
-			branch("main")
-		}
-  triggers{
-    githubPush()
-  }
-  steps{
-   
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/jenkinsci/job-dsl-plugin.git')
+          }
+          branch('*/main')
+        }
+      }
+      lightweight()
+    }
   }
 }
