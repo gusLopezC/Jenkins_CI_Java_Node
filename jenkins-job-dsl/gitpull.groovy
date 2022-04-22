fe3c48115da33e31eb6428f5a9188ab90c37e9ca
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 12:16:07 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -1,7 +1,7 @@
 job('Portafolio_backend_test'){
-    description('This job run a test in Postman with Newman')
+    description('This job will pull our files from github')
     scm {
-        git('https://github.com/manfer993/test-postman-newman.git','*/master')
+        git('https://github.com/gusLopezC/Jenkins_kube-web.git','*/main')
     }
     wrappers {
         nodejs('nodeJS_10.15.3')
@@ -9,5 +9,17 @@ job('Portafolio_backend_test'){
     steps {
         shell('npm install')
         shell('npm run test')
+       shell(''
+        '
+        ssh ec2 - user @172 .31 .74 .187 '
+        path = "/home/ec2-user/jenkins_vol/workspace/git-pull/"
+        files = $(ls $path | grep - e php - e html | wc - l) if [
+          [$files > 0]
+        ] then cp $path * /home/ec2 - user / master
+        else
+          echo "No php or html files received"
+        exit 1 fi '
+        ''
+        ')
     }
 }
