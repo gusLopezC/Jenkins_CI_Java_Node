56c9f2e8d85cb16612076052b0f356a61880cda7
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 10:38:27 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -14,5 +14,20 @@ pipelineJob('job-dsl-plugin') {
       }
       lightweight()
     }
+      steps{
+    shell('''
+ssh ec2-user@172.31.74.187 '
+path="/home/ec2-user/jenkins_vol/workspace/git-pull/"
+files=$(ls $path | grep -e php -e html | wc -l)
+if [[ $files > 0 ]]
+then
+cp $path* /home/ec2-user/master
+else
+echo "No php or html files received"
+exit 1
+fi
+'
+''')
+  }
   }
 }
