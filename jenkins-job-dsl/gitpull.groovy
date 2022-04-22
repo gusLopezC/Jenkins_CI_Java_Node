f65148d08a4d1e6f47d32f2dd2f7ccf7bd3a5f80
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 11:54:43 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -1,31 +1,14 @@
-pipelineJob('job-dsl-plugin') {
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url('https://github.com/gusLopezC/Jenkins_kube-web.git')
-          }
-          branch('*/main')
-        }
+job('job-dsl-plugin') {
+  scm {
+    git {
+      remote {
+        url('https://github.com/gusLopezC/Jenkins_kube-web.git')
       }
-      
-      lightweight()
+      branch('*/main')
     }
-      steps{
-    shell('''
-ssh ec2-user@172.31.74.187 '
-path="/home/ec2-user/jenkins_vol/workspace/git-pull/"
-files=$(ls $path | grep -e php -e html | wc -l)
-if [[ $files > 0 ]]
-then
-cp $path* /home/ec2-user/master
-else
-echo "No php or html files received"
-exit 1
-fi
-'
-''')
   }
+  steps {
+    shell('npm install')
+    shell('npm run test')
   }
 }
