358e52522fecf69f66fc46ba61137c291e353b3e
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 10:14:52 -0500

    Update gitpull.groovy

--- jenkins-job-dsl/gitpull.groovy
+++ jenkins-job-dsl/gitpull.groovy
@@ -1,8 +1,12 @@
 job("git-pull"){
   description("This job will pull our files from github")
-  scm{
-    github('gusLopezC/Jenkins_kube-web', 'main')
-  }
+  git {
+			remote {
+				github("gusLopezC/Jenkins_kube-we", "https")
+				credentials("29a0e27f-3839-46d4-a1a4-6e7fcead986d")
+			}
+			branch("main")
+		}
   triggers{
     githubPush()
   }
